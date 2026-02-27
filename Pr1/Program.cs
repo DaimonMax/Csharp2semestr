41ccd321d104a1adcff8251b9986a5826bf74bf4
DESKTOP-PGOV8S3\Admin <maxdaimon7@gmail.com>
2026-02-27 17:24:26 +0200

    as

--- Pr1/Program.cs
+++ Pr1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Pr1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pr1;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        
     }
 }
+
